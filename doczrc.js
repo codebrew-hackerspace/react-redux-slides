e5ebf19acdff1f93e0aba67d51df64347eb4520b
arshiam2 <arshiam2@illinois.edu>
2019-03-29 16:10:52 -0500

    init

---
+++ doczrc.js
@@ -0,0 +1,18 @@
+// doczrc.js
+export default {
+    dest: '/dest',
+    propsParser: false,
+  hashRouter: true,
+    themeConfig: {
+      colors: {
+        primary: '#3E9DE1',
+      },
+      styles: {
+        h1: `
+          font-size: 80px;
+          margin-bottom: 10px;
+        `,
+      },
+    },
+  }
+  
